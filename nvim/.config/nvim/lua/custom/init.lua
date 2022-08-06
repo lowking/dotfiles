09d8aa1bf839e5354a02dddf52e8222c9935fb57
lowking <lowking@sina.cn>
2022-08-06 17:29:17 +0800

    fix(nvim): format
    fix(bash): extract alias

--- nvim/.config/nvim/lua/custom/init.lua
+++ nvim/.config/nvim/lua/custom/init.lua
@@ -6,7 +6,7 @@ vim.o.rnu = true
 vim.o.scrolloff = 7
 -- Allow clipboard copy paste in neovim
 vim.g.neovide_input_use_logo = 1
-vim.api.nvim_set_keymap('', '<D-v>', '+p<CR>', { noremap = true, silent = true})
-vim.api.nvim_set_keymap('!', '<D-v>', '<C-R>+', { noremap = true, silent = true})
-vim.api.nvim_set_keymap('t', '<D-v>', '<C-R>+', { noremap = true, silent = true})
-vim.api.nvim_set_keymap('v', '<D-v>', '<C-R>+', { noremap = true, silent = true})
+vim.api.nvim_set_keymap("", "<D-v>", "+p<CR>", { noremap = true, silent = false })
+vim.api.nvim_set_keymap("!", "<D-v>", "<C-R>+", { noremap = true, silent = false })
+vim.api.nvim_set_keymap("t", "<D-v>", "<C-R>+", { noremap = true, silent = false })
+vim.api.nvim_set_keymap("v", "<D-v>", "<C-R>+", { noremap = true, silent = false })
--- nvim/.config/nvim/lua/custom/plugins/configs.lua
+++ nvim/.config/nvim/lua/custom/plugins/configs.lua
@@ -1,133 +1,133 @@
 local M = {}
 
 local function button(sc, txt, keybind)
-  local sc_ = sc:gsub("%s", ""):gsub("SPC", "<leader>")
+	local sc_ = sc:gsub("%s", ""):gsub("SPC", "<leader>")
 
-  local opts = {
-    position = "center",
-    text = txt,
-    shortcut = sc,
-    cursor = 5,
-    width = 36,
-    align_shortcut = "right",
-    hl = "AlphaButtons",
-  }
+	local opts = {
+		position = "center",
+		text = txt,
+		shortcut = sc,
+		cursor = 5,
+		width = 36,
+		align_shortcut = "right",
+		hl = "AlphaButtons",
+	}
 
-  if keybind then
-    opts.keymap = { "n", sc_, keybind, { noremap = true, silent = true } }
-  end
+	if keybind then
+		opts.keymap = { "n", sc_, keybind, { noremap = true, silent = true } }
+	end
 
-  return {
-    type = "button",
-    val = txt,
-    on_press = function()
-      local key = vim.api.nvim_replace_termcodes(sc_, true, false, true)
-      vim.api.nvim_feedkeys(key, "normal", false)
-    end,
-    opts = opts,
-  }
+	return {
+		type = "button",
+		val = txt,
+		on_press = function()
+			local key = vim.api.nvim_replace_termcodes(sc_, true, false, true)
+			vim.api.nvim_feedkeys(key, "normal", false)
+		end,
+		opts = opts,
+	}
 end
 
 M.alpha = {
-  header = {
-    val = {
-      "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⠀⠀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
-      "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⠀⠀⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
-      "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⠀⠀⣧⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
-      "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡆⠀⠀⢰⡿⠀⠀⣿⡄⠀⠀⢱⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
-      "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡼⠀⠀⢀⣿⡇⠀⠀⢹⣷⡀⠀⠈⣧⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
-      "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⡾⠁⠀⢀⣾⣿⠃⠀⠀⠸⣿⣷⠀⠀⠘⣷⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
-      "⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⡿⠁⠀⠀⣼⣿⡟⠀⠀⠀⠀⢿⣿⣧⠀⠀⠘⢿⣦⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
-      "⠀⠀⠀⠀⠀⠀⠀⠀⣠⣾⡟⠁⠀⠀⣸⣿⣿⠇⠀⠀⠀⠀⢸⣿⣿⡆⠀⠀⠈⢻⣷⡄⠀⠀⠀⠀⠀⠀⠀⠀",
-      "⠀⠀⠀⠀⠀⠀⢀⣴⣿⡏⠀⠀⠀⢀⣿⣿⣿⠀⠀⠀⠀⠀⠈⣿⣿⣿⠀⠀⠀⠀⢻⣿⣦⠀⠀⠀⠀⠀⠀⠀",
-      "⠀⠀⠀⠀⠀⢠⣾⣿⣿⠀⠀⠀⠀⣸⣿⣿⣿⠀⠀⠀⠀⠀⠀⣿⣿⣿⡇⠀⠀⠀⠀⣿⣿⣷⡀⠀⠀⠀⠀⠀",
-      "⠀⠀⠀⠀⢠⣿⣿⣿⣿⡀⠀⠀⣠⣿⣿⣿⣿⡀⠀⠀⠀⠀⢀⣿⣿⣿⣿⡄⠀⠀⢠⣿⣿⣿⣿⡀⠀⠀⠀⠀",
-      "⠀⠀⠀⠀⠈⠉⠛⢿⣿⣷⣦⠀⠀⠙⣿⣿⣿⣧⠀⠀⠀⠀⣼⣿⣿⣿⠋⠀⠀⣴⣿⣿⡿⠛⠉⠀⠀⠀⠀⠀",
-      "⠀⠀⠀⠀⠀⠀⢀⣸⣿⣿⡇⠀⠀⠀⢸⣿⣿⡏⠀⠀⠀⠀⢹⣿⣿⡇⠀⠀⠀⢸⣿⣿⣇⠀⠀⠀⠀⠀⠀⠀",
-      "⠀⠀⠀⠀⠀⠀⠀⠉⠻⣿⣧⣀⠀⠀⢿⣿⣿⣇⠀⠀⠀⠀⣼⣿⣿⡷⠀⠀⣀⣾⣿⠟⠁⠀⠀⠀⠀⠀⠀⠀",
-      "⠀⠀⠀⠀⠀⠀⠀⠀⠀⢹⣿⣿⣷⣶⣼⣿⣿⣿⣷⣄⣠⣾⣿⣿⣿⣧⣶⣾⣿⣿⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀",
-      "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠛⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠛⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
-      "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢿⣿⣿⣿⣿⣿⣿⣿⣿⡟⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
-      "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢼⣿⣿⣿⣿⣿⣿⣿⣿⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
-      "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⣿⣿⣿⣿⣿⡿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
-      "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⣿⣿⣿⣿⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
-      "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣿⣿⣿⣿⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
-      "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⣿⣿⣿⣿⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
-      "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠻⣿⣿⠟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
-      "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢻⡏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
-      "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
-    },
-  },
-  buttons = {
-    type = "group",
-    val = {
-      button("SPC f f", "  Find File  ", ":Telescope find_files<CR>"),
-      button("SPC f o", "  Recent File  ", ":Telescope oldfiles<CR>"),
-      button("SPC f w", "  Find Word  ", ":Telescope live_grep<CR>"),
-      button("SPC b m", "  Bookmarks  ", ":Telescope marks<CR>"),
-    },
-    opts = {
-      spacing = 1,
-    },
-  },
+	header = {
+		val = {
+			"⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⠀⠀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
+			"⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⠀⠀⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
+			"⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⠀⠀⣧⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
+			"⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡆⠀⠀⢰⡿⠀⠀⣿⡄⠀⠀⢱⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
+			"⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡼⠀⠀⢀⣿⡇⠀⠀⢹⣷⡀⠀⠈⣧⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
+			"⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⡾⠁⠀⢀⣾⣿⠃⠀⠀⠸⣿⣷⠀⠀⠘⣷⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
+			"⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⡿⠁⠀⠀⣼⣿⡟⠀⠀⠀⠀⢿⣿⣧⠀⠀⠘⢿⣦⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
+			"⠀⠀⠀⠀⠀⠀⠀⠀⣠⣾⡟⠁⠀⠀⣸⣿⣿⠇⠀⠀⠀⠀⢸⣿⣿⡆⠀⠀⠈⢻⣷⡄⠀⠀⠀⠀⠀⠀⠀⠀",
+			"⠀⠀⠀⠀⠀⠀⢀⣴⣿⡏⠀⠀⠀⢀⣿⣿⣿⠀⠀⠀⠀⠀⠈⣿⣿⣿⠀⠀⠀⠀⢻⣿⣦⠀⠀⠀⠀⠀⠀⠀",
+			"⠀⠀⠀⠀⠀⢠⣾⣿⣿⠀⠀⠀⠀⣸⣿⣿⣿⠀⠀⠀⠀⠀⠀⣿⣿⣿⡇⠀⠀⠀⠀⣿⣿⣷⡀⠀⠀⠀⠀⠀",
+			"⠀⠀⠀⠀⢠⣿⣿⣿⣿⡀⠀⠀⣠⣿⣿⣿⣿⡀⠀⠀⠀⠀⢀⣿⣿⣿⣿⡄⠀⠀⢠⣿⣿⣿⣿⡀⠀⠀⠀⠀",
+			"⠀⠀⠀⠀⠈⠉⠛⢿⣿⣷⣦⠀⠀⠙⣿⣿⣿⣧⠀⠀⠀⠀⣼⣿⣿⣿⠋⠀⠀⣴⣿⣿⡿⠛⠉⠀⠀⠀⠀⠀",
+			"⠀⠀⠀⠀⠀⠀⢀⣸⣿⣿⡇⠀⠀⠀⢸⣿⣿⡏⠀⠀⠀⠀⢹⣿⣿⡇⠀⠀⠀⢸⣿⣿⣇⠀⠀⠀⠀⠀⠀⠀",
+			"⠀⠀⠀⠀⠀⠀⠀⠉⠻⣿⣧⣀⠀⠀⢿⣿⣿⣇⠀⠀⠀⠀⣼⣿⣿⡷⠀⠀⣀⣾⣿⠟⠁⠀⠀⠀⠀⠀⠀⠀",
+			"⠀⠀⠀⠀⠀⠀⠀⠀⠀⢹⣿⣿⣷⣶⣼⣿⣿⣿⣷⣄⣠⣾⣿⣿⣿⣧⣶⣾⣿⣿⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀",
+			"⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠛⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠛⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
+			"⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢿⣿⣿⣿⣿⣿⣿⣿⣿⡟⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
+			"⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢼⣿⣿⣿⣿⣿⣿⣿⣿⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
+			"⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⣿⣿⣿⣿⣿⡿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
+			"⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⣿⣿⣿⣿⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
+			"⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣿⣿⣿⣿⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
+			"⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⣿⣿⣿⣿⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
+			"⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠻⣿⣿⠟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
+			"⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢻⡏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
+			"⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
+		},
+	},
+	buttons = {
+		type = "group",
+		val = {
+			button("SPC f f", "  Find File  ", ":Telescope find_files<CR>"),
+			button("SPC f o", "  Recent File  ", ":Telescope oldfiles<CR>"),
+			button("SPC f w", "  Find Word  ", ":Telescope live_grep<CR>"),
+			button("SPC b m", "  Bookmarks  ", ":Telescope marks<CR>"),
+		},
+		opts = {
+			spacing = 1,
+		},
+	},
 }
 
 M.nvimtree = {
-  git = {
-    enable = true,
-  },
-  renderer = {
-    highlight_git = true,
-    icons = {
-      show = {
-        git = true,
-      },
-    },
-  },
-  -- update_cwd = false,
-  respect_buf_cwd = false,  -- 进入目录之后，开关目录树是否还原到之前到目录
-  -- update_focused_file = {
-  --   enable = true,
-  --   update_cwd = true,
-  --   ignore_list = {},
-  -- },
-  view = {
-    adaptive_size = false,
-    side = "left",
-    width = 30,
-  },
-  -- actions = {
-  --   open_file = {
-  --     resize_window = true,
-  --   },
-  -- },
+	git = {
+		enable = true,
+	},
+	renderer = {
+		highlight_git = true,
+		icons = {
+			show = {
+				git = true,
+			},
+		},
+	},
+	-- update_cwd = false,
+	respect_buf_cwd = false, -- 进入目录之后，开关目录树是否还原到之前到目录
+	-- update_focused_file = {
+	--   enable = true,
+	--   update_cwd = true,
+	--   ignore_list = {},
+	-- },
+	view = {
+		adaptive_size = false,
+		side = "left",
+		width = 30,
+	},
+	-- actions = {
+	--   open_file = {
+	--     resize_window = true,
+	--   },
+	-- },
 }
 
 M.mason = {
-  ensure_installed = {
-    -- lua
-    "stylua",
-    "lua-language-server",
+	ensure_installed = {
+		-- lua
+		"stylua",
+		"lua-language-server",
 
-    -- web
-    "css-lsp",
-    "html-lsp",
-    "typescript-language-server",
-    "deno",
-    "emmet-ls",
-    "json-lsp",
+		-- web
+		"css-lsp",
+		"html-lsp",
+		"typescript-language-server",
+		"deno",
+		"emmet-ls",
+		"json-lsp",
 
-    -- shell
-    -- "shfmt",
-    -- "shellcheck",
+		-- shell
+		-- "shfmt",
+		-- "shellcheck",
 
-    -- yaml
-    "yamllint",
-    "yaml-language-server",
+		-- yaml
+		"yamllint",
+		"yaml-language-server",
 
-    -- python
-    "pyright",
-  },
+		-- python
+		"pyright",
+	},
 }
 
 return M
