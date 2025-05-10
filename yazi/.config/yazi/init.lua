0d114129dab296525765193a8a1afd12b3647cba
lowking <life@lowking.pro>
2025-05-10 23:11:58 +0800

    style(yazi): format

--- yazi/.config/yazi/init.lua
+++ yazi/.config/yazi/init.lua
@@ -5,10 +5,10 @@ require("smart-enter"):setup {
 require("git"):setup()
 
 require("eza-preview"):setup({
-  -- Determines the directory depth level to tree preview (default: 3)
-  level = 3,
-  -- Whether to follow symlinks when previewing directories (default: false)
-  follow_symlinks = false,
-  -- Whether to show target file info instead of symlink info (default: false)
-  dereference = false
+    -- Determines the directory depth level to tree preview (default: 3)
+    level = 3,
+    -- Whether to follow symlinks when previewing directories (default: false)
+    follow_symlinks = false,
+    -- Whether to show target file info instead of symlink info (default: false)
+    dereference = false
 })
